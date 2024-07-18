777ae17791f8ddf72f35b870aca64cd2de6fbe5e
Dylan Mullarkey <mullardt@mail.uc.edu>
2024-07-17 23:01:44 -0400

    Implement basic routing/error handling

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import ErrorPage from "./components/ErrorPage.jsx"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorPage/>
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
+{/* <App /> */}
