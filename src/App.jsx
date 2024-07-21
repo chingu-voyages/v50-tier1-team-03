b982123e5ca899269d9972d887dcaf56f2ebe2b7
Dylan Mullarkey <mullardt@mail.uc.edu>
2024-07-20 21:51:49 -0400

    Fix import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-import Header from "../components/Header.jsx"
+import Header from "./components/Header.jsx"
 import menu from "../menu.json"
 import Footer from "./components/Footer.jsx"
 import Section from "./components/Section"
