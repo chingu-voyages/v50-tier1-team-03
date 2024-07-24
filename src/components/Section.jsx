79112d49171b9a82f6098d21aa924421eeaef9d4
Michaella Rodriguez <micha.ella@hotmail.com>
2024-07-23 21:05:03 -0500

    Passed in `id` prop, for navigation

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,12 +1,12 @@
-export default function Section({ heading, content }) {
+export default function Section({ id, heading, content }) {
     return (
-        <>
+        <a id={id}>
             <h2>{heading}</h2>
             <div>
                 {content}
             </div>
             {/* menu items */}
-        </>
+        </a>
         
     )
 }
