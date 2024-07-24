9a47da5483819254ad70f301b23b20a29edd15f7
Dylan Mullarkey <mullardt@mail.uc.edu>
2024-07-23 22:49:21 -0400

    create about page component

---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,17 @@
+import Header from './Header'
+
+export default function AboutPage(){
+    return(
+        <div className='about-page-container'>
+            <Header/>
+            <div className='about-page-content'>
+                <img style={{width:"100px"}}src="src\assets\img-placeholder.png" alt="" />
+                <div>
+                    <h1>Griddle and GitHub Grill</h1>
+                    <h2>ABOUT US</h2>
+                </div>
+            </div>
+            <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Euismod luctus nostra consequat magna facilisi maximus egestas. Pellentesque et neque ornare, ut varius vestibulum tristique nisi. Lorem gravida inceptos quam proin gravida venenatis. Sodales ultricies urna sit nec vulputate habitasse. Senectus class egestas nam id auctor vulputate erat. Viverra consequat donec turpis; velit dignissim ex. Sapien potenti eu blandit venenatis erat habitasse porta sem. Diam nisi ligula, mauris vitae cubilia pulvinar. Rhoncus eget natoque praesent pretium mollis risus molestie facilisi. Sollicitudin vestibulum urna vehicula aptent libero. Inceptos morbi facilisis condimentum bibendum quam parturient eget. Eleifend quis proin morbi risus class habitant. Suspendisse placerat rutrum facilisis lacinia parturient aptent rhoncus. Suscipit eleifend laoreet sodales porta tortor et viverra montes tristique. Taciti tincidunt magna placerat amet quam interdum lorem. Tellus nunc amet platea aptent ipsum nam tortor. Justo nam malesuada netus curae ipsum dis tellus ultrices vitae. Cras sem cras odio pulvinar nulla suspendisse habitasse natoque! Molestie dapibus porta vitae mollis sed rhoncus pellentesque hendrerit. Adipiscing luctus porttitor eu neque cras cras et nascetur. Lectus phasellus lacinia nec eu, condimentum cubilia ante. Orci justo non pulvinar tempus finibus nibh non dui nibh. Potenti luctus consequat nunc odio suspendisse etiam. Quam turpis velit taciti vitae neque himenaeos magnis molestie. Finibus rutrum gravida montes neque nisi dis fusce sagittis lacus.</p>
+        </div>
+    )
+}
