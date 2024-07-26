5d0637fffcca6c6800c648e33478cfd7da5018a7
Michaella Rodriguez <micha.ella@hotmail.com>
2024-07-26 15:58:42 -0500

    used semantic elements for landmarks on the page and centered them on desktop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,12 @@ import React from "react"
 
 const Footer = () => {
     return(
-        <div className = "Footer">
+        <footer className = "Footer">
             <a href = "https://github.com/chingu-voyages/v50-tier1-team-03" target = "_blank">Repository Link</a>
             <a href = "https://www.chingu.io/privacy" target = "_blank">Primacy Terms</a>
             <h2>place holder for social media (do we need social media icon?)</h2>
             <p>© 2024 Chingu, Inc. All rights reserved</p>
-        </div>
+        </footer>
     )
 }
 
