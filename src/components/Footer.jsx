5ed374a7df95c1e1eb4cbdf3a83dd584560656e6
QH89 <60258079+QH89@users.noreply.github.com>
2024-07-28 11:30:46 -0400

    "update social media icons"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,24 @@
 import React from "react"
+import linkedin from "../assets/linkedin.png"
+import instagram from "../assets/instagram.png"
+import twitter from "../assets/twitter.png"
+import github from "../assets/github.png"
 
 const Footer = () => {
     return(
         <div className = "Footer">
-            <a href = "https://github.com/chingu-voyages/v50-tier1-team-03" target = "_blank">Repository Link</a>
+            <div className = "RepoAndPrimacy"></div>
+            <a href = "https://github.com/chingu-voyages/v50-tier1-team-03" target = "_blank">
+                Repository Link
+                <img src={github} alt={""}/>
+            </a>
             <a href = "https://www.chingu.io/privacy" target = "_blank">Primacy Terms</a>
-            <h2>place holder for social media (do we need social media icon?)</h2>
+
+            <div className = "SocialMedia">
+                <p><img src={linkedin} alt={""}/></p>
+                <p><img src={instagram} alt={""}/></p>
+                <p><img src={twitter} alt={""}/></p>
+            </div>
             <p>© 2024 Chingu, Inc. All rights reserved</p>
         </div>
     )
