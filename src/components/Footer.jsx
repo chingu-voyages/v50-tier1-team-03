647dd7fd5b2bbf8906bb8f1d076f91f236c2f3f7
QH89 <60258079+QH89@users.noreply.github.com>
2024-07-28 11:39:02 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,21 @@ import React from "react"
 
 const Footer = () => {
     return(
-        <footer className = "Footer">
-            <a href = "https://github.com/chingu-voyages/v50-tier1-team-03" target = "_blank">Repository Link</a>
+        <div className = "Footer">
+            <div className = "RepoAndPrimacy"></div>
+            <a href = "https://github.com/chingu-voyages/v50-tier1-team-03" target = "_blank">
+                Repository Link
+                <img src={github} alt={""}/>
+            </a>
             <a href = "https://www.chingu.io/privacy" target = "_blank">Primacy Terms</a>
-            <h2>place holder for social media (do we need social media icon?)</h2>
+
+            <div className = "SocialMedia">
+                <p><img src={linkedin} alt={""}/></p>
+                <p><img src={instagram} alt={""}/></p>
+                <p><img src={twitter} alt={""}/></p>
+            </div>
             <p>© 2024 Chingu, Inc. All rights reserved</p>
-        </footer>
+        </div>
     )
 }
 
