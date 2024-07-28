fdb80ced23df3ba3fa7c22e50a8f30a47b242ff5
QH89 <60258079+QH89@users.noreply.github.com>
2024-07-28 13:04:01 -0400

    "update footer styling, changed background colors"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,31 @@
 import React from "react"
+import github from "../assets/github.png"
+import linkedin from "../assets/linkedin.png"
+import twitter from "../assets/twitter.png"
 
 const Footer = () => {
     return(
         <div className = "Footer">
-            <div className = "RepoAndPrimacy"></div>
-            <a href = "https://github.com/chingu-voyages/v50-tier1-team-03" target = "_blank">
-                Repository Link
-                <img src={github} alt={""}/>
-            </a>
-            <a href = "https://www.chingu.io/privacy" target = "_blank">Primacy Terms</a>
-
-            <div className = "SocialMedia">
-                <p><img src={linkedin} alt={""}/></p>
-                <p><img src={instagram} alt={""}/></p>
-                <p><img src={twitter} alt={""}/></p>
+            <link rel="stylesheet" href="src/index.css"></link>
+            <div className = "FooterLink">
+                <h3>Source Code</h3>
+                <a href = "https://github.com/chingu-voyages/v50-tier1-team-03" style={{color: "white"}}target = "_blank">
+                    Repository Link
+                <img src={github} style={{width:"5%"}} alt={""}/>
+                </a>
             </div>
-            <p>© 2024 Chingu, Inc. All rights reserved</p>
+            <div className ="FooterLink">
+                <h3>Terms and Privacy</h3>
+                <a href = "https://www.chingu.io/privacy" style={{color: "white"}} target = "_blank">Primacy Terms</a>
+            </div>
+            <div className = "FooterLink">
+                <h3>Social Media</h3>
+                <a href="https://www.linkedin.com/company/chingu-os/"><img src={linkedin} style={{width:"3%"}} alt={""}/></a>
+                <a href="https://twitter.com/ChinguCollabs"><img src={twitter} style={{width:"3%"}} alt={""}/></a>
+            </div>
+            <div className = "FooterNote">
+                <p>© 2024 Chingu, Inc. All rights reserved</p>
+            </div>
         </div>
     )
 }
