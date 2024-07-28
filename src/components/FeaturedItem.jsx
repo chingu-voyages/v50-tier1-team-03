22e46459aa917e9f4248c7cdbd103d8e0b14f4cd
QH89 <60258079+QH89@users.noreply.github.com>
2024-07-28 14:01:01 -0400

    Update FeaturedItem.jsx

--- src/components/FeaturedItem.jsx
+++ src/components/FeaturedItem.jsx
@@ -5,7 +5,7 @@ const FeaturedItem = () => {
     return(
         <section className="featured-item">
             <div className="item-image">
-               <img src="../src/assets/shredded-shroom.png" alt="Shredded Shroom Sandwich" className="featured--item__image"></img>
+               <img src="src/assets/shredded-shroom.png" alt="Shredded Shroom Sandwich" className="featured--item__image"></img>
             </div>
             <div className="item-description">
                 <h2>Item of the Month!</h2>
