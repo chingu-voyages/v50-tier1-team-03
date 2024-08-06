6c26d5cf2a267db6dd978e48037ad326db10b6d2
Dylan Mullarkey <mullardt@mail.uc.edu>
2024-08-06 18:18:37 -0400

    Pass current item and state to modifications component

--- src/components/menuComponents/MenuItemPage.jsx
+++ src/components/menuComponents/MenuItemPage.jsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom"
+import { useState } from "react"
 import menu from "../../../menu.json"
 import Modifications from './Modifications'
 import Options from './Options'
@@ -7,6 +8,12 @@ import AmountAddToCart from './AmountAddToCart'
 // item state- we could raise the item state up one component into the app. that way, 
 // each window does not have it's own state. it's managed in one place.
 export default function MenuItemPage(){
+    const [item, setItem] = useState({
+        modifications:[], 
+        addOns:[],
+        amount: 0,
+    })
+
     const param = useParams()
 
     function isItem(item){
@@ -14,7 +21,6 @@ export default function MenuItemPage(){
     }
 
     const menuItem = menu.meals.find(isItem) || menu.drinks.find(isItem)|| menu.desserts.find(isItem)
-    
     return(
         <main className="menu-item-page">
             {/* Instead of hard-coded values, use props*/}
@@ -23,7 +29,7 @@ export default function MenuItemPage(){
             <h2>{menuItem.name}</h2>
             <p>{menuItem.description}</p>
 
-            <Modifications />
+            <Modifications selectedItem={menuItem} item={item}/>
 
             <Options />
 
