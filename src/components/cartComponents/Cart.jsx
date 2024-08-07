7bb00f1652c9cec196eeedf14aa750855f1d746c
Michaella Rodriguez <micha.ella@hotmail.com>
2024-08-07 18:53:00 -0500

    added button styling

--- src/components/cartComponents/Cart.jsx
+++ src/components/cartComponents/Cart.jsx
@@ -9,20 +9,20 @@ export default function Cart() {
                     <h2>Item name <span id="quantity"></span></h2>
                     <p>$ (Item price)</p>
                 </div>
-                <button>Edit</button>
-                <button>Remove</button>
+                <button className="cart-btn">Edit</button>
+                <button className="cart-btn">Remove</button>
                 <div className="cart-item">
                     <h2>Item name <span id="quantity"></span></h2>
                     <p>$ (Item price)</p>
                 </div>
-                <button>Edit</button>
-                <button>Remove</button>
+                <button className="cart-btn">Edit</button>
+                <button className="cart-btn">Remove</button>
                 <div className="cart-item">
                     <h2>Item name <span id="quantity"></span></h2>
                     <p>$ (Item price)</p>
                 </div>
-                <button>Edit</button>
-                <button>Remove</button>
+                <button className="cart-btn">Edit</button>
+                <button className="cart-btn">Remove</button>
                 <div className="checkout-el">
                     <p>Total: <span id="total-el">$50.00</span></p>
                     <button className="checkout-btn">Check out</button>
