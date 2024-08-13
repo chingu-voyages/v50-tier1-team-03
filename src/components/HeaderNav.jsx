d6720767971c7335094882e567a1d62b8bb581d3
Dylan Mullarkey <mullardt@mail.uc.edu>
2024-08-13 18:39:27 -0400

    Render cart length

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -2,18 +2,15 @@
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from "react";
 import { Link } from "react-router-dom";
-import { useCart } from "../CartState"
 
 const HeaderNav = () => {
-    const [cart, setCart] = useCart()
-
     return(
         <nav>
             <ul>
                 <li><Link to="#" className="header--nav__link">About</Link></li>
                 <li><Link to="#" className="header--nav__link">Contact</Link></li>
                 <li><Link to="#" className="header--nav__link">Add Credits</Link></li>
-                <li><Link to="/Cart" className="header--nav__link">Cart | {cart.length}</Link></li>
+                <li><Link to="/Cart" className="header--nav__link">Cart | {JSON.parse(localStorage.getItem("cart")).length}</Link></li>
                     {/* Number (3) above is placeholder for now. Will update to change with amount of items in cart*/}
             </ul>
         </nav>
