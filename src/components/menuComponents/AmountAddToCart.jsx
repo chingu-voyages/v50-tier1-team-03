9d3b81c7e85337d6713385eac478c65b26003eae
Dylan Mullarkey <mullardt@mail.uc.edu>
2024-08-13 12:58:10 -0400

    clean imports

--- src/components/menuComponents/AmountAddToCart.jsx
+++ src/components/menuComponents/AmountAddToCart.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom"
 import menu from "../../../menu.json"     
 import { useCart } from "../../CartState"
 
 export default function AmountAddToCart({item, setItem, menuItem}) {
 
-    console.log(cart)
     function handleAmountDecrease(){
         if (item.amount === 0){
             return
