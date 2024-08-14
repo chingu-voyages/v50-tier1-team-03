674b0705c848c00bad2f7f344c5d22b9a3e3d428
ken_ellingsen <ken.ellingsen@gmail.com>
2024-08-14 12:08:16 -0500

    Added structure to Checkout page

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -7,8 +7,29 @@ export default function Checkout() {
     return (
         <>
         <Header />
+        <section className="checkout-section">
 
+            {/* Display subtotal */}
+            <div className="subtotal">
+                
+            </div>
 
+            {/* Add tip */}
+            <div className="add-tip">
+
+            </div>
+
+            {/* Add credit */}
+            <div className="add-credit">
+
+            </div>
+            
+            {/* Checkout */}
+            <div className="checkout-final">
+
+            </div>
+
+        </section> 
         <Footer />
         </>
     )
