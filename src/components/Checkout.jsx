274b1d38da14afd5fb56463c14025b6bcd31404f
ken_ellingsen <ken.ellingsen@gmail.com>
2024-08-14 11:53:27 -0500

    Added checkout page

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function Checkout() {
+
+
+    return (
+        <>
+        <Header />
+
+
+        <Footer />
+        </>
+    )
+}
