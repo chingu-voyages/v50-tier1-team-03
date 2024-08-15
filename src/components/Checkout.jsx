f98f4852ec039e33afcf0702ef2d86157acd36b0
ken_ellingsen <ken.ellingsen@gmail.com>
2024-08-15 16:52:47 -0500

    Added divs for confirmation/rejection of payment messages and function to reveal each one respectively

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,10 +1,30 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import { Link } from "react-router-dom";
 
 export default function Checkout() {
 
+    // Variables for checkBalance fn
+    const success = document.querySelector('.payment-success');
+    const fail = document.querySelector('.payment-fail');
+
     let subtotal = 0;
+    let tip = 0;
+
     let credit = 0;
+
+    function addCredit() {
+
+    }
+
+    function checkBalance() {
+        if (subtotal >= credit){
+            credit -= subtotal;
+            success.classList.remove('hidden');
+        } else {
+            fail.classList.remove('hidden');
+        }
+    }
 
     return (
         <>
@@ -33,13 +53,22 @@ export default function Checkout() {
                 <form>
                     <label>Enter credit:</label><br />
                     <input className="credit-input" type="text" /><br />
-                    <input className="submit-btn" type="submit" value="Add Credit" />
+                    <input className="submit-btn" type="submit" value="Add Credit" onClick={addCredit} />
                 </form>
             </div>
             
             {/* Checkout */}
             <div className="checkout-final">
-                <button className="checkout-btn-final">Check Out</button>
+                <button className="checkout-btn-final" onClick={checkBalance}>Check Out</button>
+            </div>
+
+                    {/* Hidden in CSS by default*/}
+            <div className={'payment-success hidden'}>
+                <p>Payment successful!</p>
+                <Link to="/"><button className="menu-return-btn">Return to Menu</button></Link>
+            </div>
+            <div className={'payment-fail hidden'} >
+                <p>Insufficient balance, please add more credit.</p>
             </div>
 
         </section> 
